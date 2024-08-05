91a1a151b7ce5468746763f6647c937d643b6605
David-0102 <Davidmumondunda@gmail.com>
2024-08-05 16:56:27 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,49 +1,68 @@
 #!/usr/bin/python3
 """
-s script fetches and displays the TODO list progress for a given employee ID.
+fetches and displays the TODO list progress for a given employee ID.
 
 Usage:
     ./0-gather_data_from_an_API.py <employee_id>
 
 Arguments:
-    employee_id (int): The ID of the employee whose TODO list progress is to be retrieved.
+    employee_id (int): The ID of the employee whose TODO list is to be checked.
 
 The script performs the following tasks:
 1. Fetches the employee's details from the API.
 2. Retrieves the TODO list for the specified employee.
 3. Calculates the number of completed tasks and total tasks.
-4. Prints out the employee's name and their TODO list progress in a formatted manner.
+4. Prints out the employee's name and their TODO list.
 """
+
 import requests
 import sys
 
+
 def gather_data(employee_id):
+    """
+    Fetches and displays the TODO list progress for the specified employee ID.
+
+    Args:
+        employee_id (int): The ID whose TODO list progress is to be cheked.
+
+    Returns:
+        None: Prints the TODO list progress to the standard output.
+    """
     base_url = "https://jsonplaceholder.typicode.com"
-    
+
     # Fetch employee details
     user_url = f"{base_url}/users/{employee_id}"
     user_response = requests.get(user_url)
     if user_response.status_code != 200:
         print(f"User with ID {employee_id} not found.")
         return
-    
+
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
     # Fetch employee TODO list
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+    todos_url = (
+        f"{base_url}/todos?userId={employee_id}"
+    )
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
 
     # Process TODO list
     total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get("completed")]
+    done_tasks = [
+        task for task in todos_data if task.get("completed")
+    ]
     number_of_done_tasks = len(done_tasks)
 
     # Display the results
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    print(
+        f"Employee {employee_name} is done with tasks("
+        f"{number_of_done_tasks}/{total_tasks}):"
+    )
     for task in done_tasks:
         print(f"\t {task.get('title')}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -54,4 +73,3 @@ if __name__ == "__main__":
             gather_data(employee_id)
         except ValueError:
             print("Employee ID must be an integer.")
-
