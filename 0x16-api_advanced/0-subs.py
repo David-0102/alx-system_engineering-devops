deb8153c9ddb81eb720334cbc29fc94546f8e4ce
David-0102 <davidmumondunda@gmail.com>
2024-09-01 13:59:18 -0700

    Fix issues with 0-subs.py for project 0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,12 @@ Queries the Reddit API and returns the number of subscribers.
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
+
+    """Queries the Reddit API and returns the number of subscribers."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent 1.0'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
@@ -15,6 +18,6 @@ def number_of_subscribers(subreddit):
             return data.get('subscribers', 0)
         else:
             return 0
-    except Exception:
+    except requests.RequestException:
         return 0
 
